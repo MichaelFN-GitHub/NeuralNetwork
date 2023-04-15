406cd5fb817494ea9322fc807772a38c11e86161
MHVandborg <mvandborg@live.dk>
2023-04-15 19:47:48 +0200

    Create cartpole.py

---
+++ cartpole.py
@@ -0,0 +1,22 @@
+'''
+    This file is just to follow the implementation of a cartpole, which
+    is a cart trying to balance a standing pole.
+    
+    Link:
+    https://www.janisklaise.com/post/rl-policy-gradients/
+'''
+
+import gym
+
+env = gym.make('CartPole-v0')
+
+print(f'Observation Space\n{env.observation_space}\n')
+
+# print(f'Box\n{Box4,}\n')
+
+print(f'Action Space\n{env.action_space}\n')
+
+# print(f'Discrete\n{Discrete(2)}\n')
+
+
+
