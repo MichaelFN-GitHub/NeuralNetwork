5d3d419c5f8e29218a74c87c5ce0eb55a53ad1bd
MHVandborg <mvandborg@live.dk>
2023-04-15 11:02:31 +0200

    Update links.py

--- links.py
+++ links.py
@@ -8,6 +8,21 @@
 
 # %% Gradient Policy Implementation
 
-# 
+# Good walkthrough of gradient policy with python implementation
 # https://www.janisklaise.com/post/rl-policy-gradients/
 
+
+# Maybe links?
+# https://minpy.readthedocs.io/en/latest/tutorial/rl_policy_gradient_tutorial/rl_policy_gradient.html
+# https://huggingface.co/blog/deep-rl-pg
+# https://towardsdatascience.com/snake-with-policy-gradients-deep-reinforcement-learning-5e6e921db054
+# https://www.sefidian.com/2021/10/01/policy-g/
+# https://pylessons.com/Beyond-DQN
+
+
+
+
+# %% Future Ideas
+
+# Balancing Pole
+# https://huggingface.co/blog/deep-rl-pg
