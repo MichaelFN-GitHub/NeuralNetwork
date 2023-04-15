8e5f17dd1efb454607ff646e30d297ae3751d817
MHVandborg <mvandborg@live.dk>
2023-04-15 09:56:47 +0200

    Links file
    Added file with useful links

---
+++ links.py
@@ -0,0 +1,13 @@
+# %% Neural Network Refinforced Learning Implementation
+
+# Notebook with formulas and python implementation
+# https://www.kaggle.com/code/wwsalmon/simple-mnist-nn-from-scratch-numpy-no-tf-keras/notebook
+
+
+
+
+# %% Gradient Policy Implementation
+
+# 
+# https://www.janisklaise.com/post/rl-policy-gradients/
+
