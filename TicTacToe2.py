b4167f0c4fbb6ed36f24764bd1cbaa6bc5b4a0ab
MHVandborg <mvandborg@live.dk>
2023-10-19 10:20:18 +0200

    Updated to PyCharm syntax

--- TicTacToe2.py
+++ TicTacToe2.py
@@ -1,4 +1,7 @@
 import numpy as np
+import time
+import random
+
 
 class TicTacToe:
     """
@@ -15,47 +18,52 @@ class TicTacToe:
             - X -
             - - -
     """
-    
+
     PLAYER_WHITE = 0
     PLAYER_BLACK = 9
-    
+
     WHITE_WIN = 1
     BLACK_WIN = -1
     DRAW = 0
-    
-    
+
     # Constructor
     def __init__(self):
+        # All variables in this class
+        self.board = None
+        self.player = None
+        self.legal_moves = None
+        self.game_over = None
+        self.one_hot_state = None
+        self.one_hot_input = None
+        self.winner = None
+
+        # Overwrite variables
         self.restart_game()
-        
-    
-    # Retarts the game
+
+    # Restarts the game
     def restart_game(self):
-        self.board = 0                     #Init empty board
-        self.player = self.PLAYER_WHITE    #Init first player
-        self.legal_moves = [0,1,2,3,4,5,6,7,8]
+        self.board = 0  # Init empty board
+        self.player = self.PLAYER_WHITE  # Init first player
+        self.legal_moves = [0, 1, 2, 3, 4, 5, 6, 7, 8]
         self.game_over = False
-        self.one_hot_state = np.zeros((9,1))
+        self.one_hot_state = np.zeros((9, 1))
         self.one_hot_input = 1
-        
-        
+        self.winner = None
+
     # Get one hot state
     def get_state(self):
         return self.one_hot_state
-        
-        
+
     # Get legal moves
     def get_legal_moves(self):
         return self.legal_moves
-        
-    
+
     # Makes move by current player on the given square
     def make_move(self, move):
-        
         # Return if game is already over
-        if (self.game_over):
+        if self.game_over:
             return False
-        
+
         """
         Player black is set to 9, so that the
         move can be easily shifted to the right bit:
@@ -64,127 +72,115 @@ class TicTacToe:
         """
         # Move the move on the board
         self.board |= (1 << (move + self.player))
-        
+
         # Update legal moves
         self.legal_moves.remove(move)
-        
+
         # Update one hot state
         self.one_hot_state[move] = self.one_hot_input
 
         # Check for win
         self.game_over = self.check_for_win()
-        
+
         # Change player
         self.change_player()
-        
-        
+
         return True
-    
-    
+
     # Check if game is over
     def check_for_win(self):
         win_combinations = [
             0b111000000, 0b000111000, 0b000000111,  # Rows
             0b100100100, 0b010010010, 0b001001001,  # Columns
-            0b100010001, 0b001010100                # Diagonals
-            ]
-        
-        # Get bit board for each player
+            0b100010001, 0b001010100  # Diagonals
+        ]
+
+        # Get bit-board for each player
         white_board = self.board & 0b111111111
         black_board = (self.board >> self.PLAYER_BLACK) & 0b111111111
-        
+
         # Check every win combination and return winner if found
         for combination in win_combinations:
-            if ((white_board & combination) == combination):
+            if (white_board & combination) == combination:
                 self.winner = self.WHITE_WIN
                 return True
-            elif ((black_board & combination) == combination):
+            elif (black_board & combination) == combination:
                 self.winner = self.BLACK_WIN
                 return True
-        
-        # Draw if no win comibnation was found and board is full
-        if ((white_board | black_board) == 0b111111111):
+
+        # Draw if no win combination was found and board is full
+        if (white_board | black_board) == 0b111111111:
             self.winner = self.DRAW
             return True
-        
+
         # No combination was found and board not full -> game is not over
         return False
-        
-        
+
     # Changes player
     def change_player(self):
         self.player = self.PLAYER_BLACK - self.player
         self.one_hot_input = -self.one_hot_input
-        
-        
+
     # Print the board
     def __str__(self):
         s = "\n"
-        
+
         # Print winner if game over, else prints player to move
-        if (self.game_over):
+        if self.game_over:
             s += "Game over."
-        
-            
+
         # Print the board
-        for i in range(9):
-            
+        for tile in range(9):
+
             # New line every 3 squares
-            if (i%3 == 0):
+            if tile % 3 == 0:
                 s += "\n"
-              
+
             # Add "X" if white, "O" if black, "-" if empty
-            if ((self.board >> (self.PLAYER_WHITE + i)) & 1 == 1):
+            if (self.board >> (self.PLAYER_WHITE + tile)) & 1 == 1:
                 s += "X"
-            elif ((self.board >> (self.PLAYER_BLACK + i)) & 1 == 1):
+            elif (self.board >> (self.PLAYER_BLACK + tile)) & 1 == 1:
                 s += "O"
             else:
                 s += "-"
-                
+
         return s
 
 
-# %% Test game and print board
-import random
+if __name__ == "__main__":
+    # %% Test game and print board
+    game = TicTacToe()
 
-game = TicTacToe()
-
-for i in range(9):
-    legal_moves = game.get_legal_moves()
-    if not game.game_over:
-        random.shuffle(legal_moves)
-        game.make_move(legal_moves[0])
-    print(game)
-print(game.get_state())
-
-
-# %% Check time to play 1 million games
-import time
-import random
-
-game = TicTacToe()
-
-start_time = time.time()
-num_games = 1000000
-for i in range(num_games):
-    
-    if (i%100000 == 0):
-        elapsed_time = time.time() - start_time
-        print(f"{i} games played in {elapsed_time} seconds")
-    
-    game.restart_game()
     for i in range(9):
         legal_moves = game.get_legal_moves()
         if not game.game_over:
             random.shuffle(legal_moves)
             game.make_move(legal_moves[0])
+        print(game)
+    print(game.get_state())
 
-elapsed_time = time.time() - start_time
-print(f"Time to play 1 million games: {elapsed_time} seconds")
+    # %% Check time to play 1 million games
+    game = TicTacToe()
 
-# %% Test
-import random
+    start_time = time.time()
+    num_games = 1000000
+    for i in range(num_games):
 
-game = TicTacToe()
-legal_moves = game.get_legal_moves()
-random.shuffle(legal_moves)
+        if i % 100000 == 0:
+            elapsed_time = time.time() - start_time
+            print(f"{i} games played in {elapsed_time} seconds")
+
+        game.restart_game()
+        for _ in range(9):
+            legal_moves = game.get_legal_moves()
+            if not game.game_over:
+                random.shuffle(legal_moves)
+                game.make_move(legal_moves[0])
+
+    elapsed_time = time.time() - start_time
+    print(f"Time to play 1 million games: {elapsed_time} seconds")
+
+    # %% Test
+    game = TicTacToe()
+    legal_moves = game.get_legal_moves()
+    random.shuffle(legal_moves)
